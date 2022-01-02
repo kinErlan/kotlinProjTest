bb0f6c65025d043e237597fcf88706d08bd97ee2
kinErlan <imaniyazov@gmail.com>
2022-01-02 15:25:13 +0600

    5 chapter simVillage 2

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,19 +1,20 @@
 fun main(){
-
-    runSimulation("erl", ::ptintConstructionCost) { playerName, numBuilding->
+    val greetingFunction = { playerName: String, numBuilding: Int ->
         val currentYear = 2022
         println("adding $numBuilding houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
+    runSimulation("erl", ::printConstructionCost, greetingFunction)
 }
 
 inline fun runSimulation(playerName: String, costPrinter: (Int)->Unit, greetingFunction: (String, Int)->String){
     val numBuilding = (1..3).shuffled().last()
-    costPrinter(numBuilding)
+   costPrinter(numBuilding)
     println(greetingFunction(playerName,numBuilding))
 }
 
-fun ptintConstructionCost(numBuildings:Int) {
+fun printConstructionCost(numBuildings:Int) {
     val cost = 500
     println("construction cost: ${cost * numBuildings}")
 }
+
