613f8e1fd24d07351c1ea94cea89438d8c5ad1e5
kinErlan <imaniyazov@gmail.com>
2022-01-02 18:24:21 +0600

    5 chapter simVillage тип функции как возвращаемый тип

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,20 +1,20 @@
 fun main(){
-    val greetingFunction = { playerName: String, numBuilding: Int ->
+    runSimulation()
+}
+
+inline fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Erlan"))
+    println(greetingFunction("Erlan2"))
+}
+
+fun configureGreetingFunction(): (String)->String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return {playerName: String ->
         val currentYear = 2022
-        println("adding $numBuilding houses")
-        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+        numBuildings += 1
+        println("adding $numBuildings $structureType")
+        "welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-    runSimulation("erl", ::printConstructionCost, greetingFunction)
 }
-
-inline fun runSimulation(playerName: String, costPrinter: (Int)->Unit, greetingFunction: (String, Int)->String){
-    val numBuilding = (1..3).shuffled().last()
-   costPrinter(numBuilding)
-    println(greetingFunction(playerName,numBuilding))
-}
-
-fun printConstructionCost(numBuildings:Int) {
-    val cost = 500
-    println("construction cost: ${cost * numBuildings}")
-}
-
