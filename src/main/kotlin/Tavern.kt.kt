503e5b4f382d325c055a15be074d9a77d04738d2
kinErlan <imaniyazov@gmail.com>
2022-01-03 15:55:01 +0600

    6 chapter Tavern

---
+++ src/main/kotlin/Tavern.kt.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    println("beverage 1")
+    var beverage = readLine()?.let {
+        if(it.isNotBlank()) {
+            it.replaceFirstChar { it.uppercase() }
+        } else {
+            "Buttered Ale"
+        }
+    }
+    println(beverage)
+
+    println("beverage 2")
+    var beverage2 = readLine()!!.replaceFirstChar { it.uppercase() }
+    println(beverage2)
+
+    println("beverage 3")
+    var beverage3 = readLine()
+    beverage3 = beverage3?.replaceFirstChar { it.uppercase() }?.plus("!!!!!")
+    println(beverage3)
+
+    println("beverage 4")
+    var beverage4 = readLine()
+    val beverageServed: String = beverage4 ?: "buttered ale"
+    println(beverage4)
+}
