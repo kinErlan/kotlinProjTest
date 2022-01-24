4305eac9eb3aa99659846a6229a66734b418a60d
kinErlan <imaniyazov@gmail.com>
2022-01-24 19:20:46 +0600

    тест. проценты по ипотеке

---
+++ src/main/kotlin/procenty.kt
@@ -0,0 +1,30 @@
+import kotlin.math.roundToInt
+
+fun main(){
+    val obzhaySumma = 9500000 // s
+    var stavka = 14.9 / (100.0 * 12.0) // p
+//    var stavka = 11.3 / (100.0 * 12.0) // p
+    var ezhemecyachnayaOplata = 0.0
+    val srokKredita = 180 // m
+    var pereplata = 0.0
+
+    
+//    ezhemecyachnayaOplata = 1 + stavka
+//
+//    val stepen = ezhemecyachnayaOplata
+//    for(i in 2 .. srokKredita) {
+//        ezhemecyachnayaOplata *= stepen
+//    }
+//
+//    ezhemecyachnayaOplata = 1 - ( 1 / ezhemecyachnayaOplata)
+//
+//    ezhemecyachnayaOplata = obzhaySumma * (0.01 / ezhemecyachnayaOplata)
+//    ezhemecyachnayaOplata -= ezhemecyachnayaOplata % 1.0
+//    println("ezhemecyachnayaOplata - $ezhemecyachnayaOplata")
+//
+//    pereplata = (ezhemecyachnayaOplata * srokKredita) - obzhaySumma
+//    println("pereplata - $pereplata")
+
+
+
+}
